d827d15e4fcf3d48ea0ab17e6b5bfbe2220a634f
sherrys2025 <114745340+sherrys2025@users.noreply.github.com>
2023-01-19 10:21:25 +0800

    Added Cube class

---
+++ src/Cube.java
@@ -0,0 +1,37 @@
+public class Cube {
+    private int side;
+
+    public Cube(){
+        side = 1;
+    }
+
+    public Cube(int side){
+        if (side < 1)
+            throw new IllegalArgumentException("A cube’s side length cannot be less than 1!");
+        else
+            setSide(side);
+    }
+
+    public int getSide(){
+        return side;
+    }
+    public void setSide(int side){
+        if (side<1)
+            throw new IllegalArgumentException("A cube’s side length cannot be less than 1!");
+        this.side = side;
+    }
+
+    public int getSurfaceArea(){
+        return side*side*6;
+    }
+
+    public int getVolume(){
+        return side*side*side;
+    }
+
+    public String toString(){
+        return "Cube{side=" + side + "}";
+    }
+
+
+}
